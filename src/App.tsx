086edc01ff199a130c13db6924def02a419e7f37
Lucas Reis <lucas@amarok.kr>
2024-02-10 09:45:02 +0100

    add TS entrypoint

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import ProjectWizard from 'components/ProjectWizard';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        Map View
+      </header>
+      <ProjectWizard />
+    </div>
+  );
+}
+
+export default App;
